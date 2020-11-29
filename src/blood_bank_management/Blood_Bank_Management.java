c3334c1d3cd24717b339d60eb58be267224a01ef
ABHIRAM <abhirampalika@gmail.com>
2020-11-29 12:52:42 +0530

    hello

---
+++ src/blood_bank_management/Blood_Bank_Management.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blood_bank_management;
+
+/**
+ *
+ * @author ABHIRAM
+ */
+public class Blood_Bank_Management {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
